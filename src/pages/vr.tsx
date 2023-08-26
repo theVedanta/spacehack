7ccbdb913bc5aa2e4e2fead2173be6c279f7d253
Vedanta Somnathe <vedanta1412@gmail.com>
2023-08-26 07:40:31 +0530

    night scene

--- src/pages/vr.tsx
+++ src/pages/vr.tsx
@@ -15,47 +15,12 @@ import {
 const VR = () => {
     return (
         <div className="vr">
-            <XRCoding />
+            <CanvasCollection />
         </div>
     );
 };
 
-const XRCoding = () => {
-    return (
-        <>
-            <VRButton />
-            <Canvas>
-                <XR>
-                    <Controllers />
-                    <Hands />
-                    <mesh>
-                        <boxGeometry />
-                        <meshBasicMaterial color="blue" />
-                    </mesh>
-                </XR>
-            </Canvas>
-        </>
-    );
-};
-
 const CanvasCollection = () => {
-    // const {
-    //     // An array of connected `XRController`
-    //     controllers,
-    //     // Whether the XR device is presenting in an XR session
-    //     isPresenting,
-    //     // Whether hand tracking inputs are active
-    //     isHandTracking,
-    //     // A THREE.Group representing the XR viewer or player
-    //     player,
-    //     // The active `XRSession`
-    //     session,
-    //     // `XRSession` foveation. This can be configured as `foveation` on <XR>. Default is `0`
-    //     foveation,
-    //     // `XRSession` reference-space type. This can be configured as `referenceSpace` on <XR>. Default is `local-floor`
-    //     referenceSpace,
-    // } = useXR();
-
     return (
         <div className="main">
             <Canvas>
@@ -64,36 +29,12 @@ const CanvasCollection = () => {
                 <pointLight position={[10, 10, 10]} />
                 <directionalLight color="white" position={[0, 0, 5]} />
 
-                {/* <OrbitControls /> */}
-
-                <XR
-                    foveation={0}
-                    frameRate={72 | 90 | 120}
-                    referenceSpace="local-floor"
-                    onSessionStart={(event: XREvent<XRManagerEvent>) => {}}
-                    onSessionEnd={(event: XREvent<XRManagerEvent>) => {}}
-                    onVisibilityChange={(event: XREvent<XRSessionEvent>) => {}}
-                    onInputSourcesChange={(
-                        event: XREvent<XRSessionEvent>
-                    ) => {}}
-                >
-                    <Controllers
-                        /** Optional material props to pass to controllers' ray indicators */
-                        rayMaterial={{ color: "blue" }}
-                        /** Whether to hide controllers' rays on blur. Default is `false` */
-                        hideRaysOnBlur={false}
-                    />
-                    <Hands
-                    // // Optional custom models per hand. Default is the Oculus hand model
-                    // modelLeft="/hand-left.glb"
-                    // modelRight="/hand-right.glb"
-                    />
-                </XR>
+                <OrbitControls />
 
                 {/* GEOMETRY */}
                 {/* <Model /> */}
                 <Environment preset="night" background />
-                {/* <Satellite /> */}
+                <Satellite />
             </Canvas>
         </div>
     );
